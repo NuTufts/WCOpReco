51e59ad64f86764ff4531026691b3bc28b4b93bf
Joshua Mills <joshua.mills@tufts.edu>
2018-09-17 15:16:51 -0400

    woops!

--- src/data/Opflash.cxx
+++ src/data/Opflash.cxx
@@ -1,68 +1,178 @@
-#ifndef Opflash_h
-#define Opflash_h
+#include "WireCellData/Opflash.h"
+#include <algorithm>
+#include <iostream>
 
-#include "COphit.h"
-#include "TH1F.h"
-#include <set>
+using namespace WireCell;
 
-namespace WireCell{
-  class Opflash{
-  public:
-    Opflash(COphitSelection &ophits);
-    Opflash(TH1F **hist, double start_time, int start_bin, int end_bin, float bin_width=6*15.625/1000.);
-    ~Opflash();
+WireCell::Opflash::Opflash(COphitSelection &ophits)
+  : type(1)
+  , flash_id (-1)
+{
+  for (int i=0;i!=32;i++){
+    PE[i] = 0;
+    PE_err[i] = 6.4; // 11/sqrt(3.) fudge now ...
+  }
+  time = 0;
+  total_PE = 0;
+  for (size_t i=0; i!= ophits.size(); i++){
+    fired_channels.push_back(ophits.at(i)->get_ch_no());
+    PE[ophits.at(i)->get_ch_no()] = ophits.at(i)->get_PE() - 0.15*2; // 250 kHz at 0.6 us ...
+    PE_err[ophits.at(i)->get_ch_no()] = ophits.at(i)->get_PE_err();
+    time += ophits.at(i)->get_PE() * ophits.at(i)->get_time();
+    total_PE += ophits.at(i)->get_PE() ;
+  }
 
-    void Add_l1info(TH1F* hist1, TH1F *hist2, double start_time , int start_bin, int end_bin, float bin_width=6*15.625/1000.);
+  // std::cout << total_PE << std::endl;
+  if (total_PE !=0){
+    time /= total_PE;
+  }else{
+    time = ophits.at(0)->get_time();
+  }
 
-    void set_flash_id(int value){flash_id = value;};
-    int get_flash_id(){return flash_id;};
-
-    double get_time(){return time;};
-    double get_total_PE(){return total_PE;};
-    double get_PE(int ch){return PE[ch];};
-    double get_PE_err(int ch){return PE_err[ch];};
-    bool get_fired(int ch);
-    int get_num_fired(){return fired_channels.size();};
-
-    int get_type(){return type;}
-    double get_low_time(){return low_time;};
-    double get_high_time(){return high_time;};
-
-    std::vector<double>& get_l1_fired_time(){return l1_fired_time;};
-    std::vector<double>& get_l1_fired_pe(){return l1_fired_pe;};
-
-    void swap_channels();
-
-  protected:
-
-    int type;
-    int flash_id;
-    double low_time;
-    double high_time;
-
-    double time;
-    double total_PE;
-    std::vector<int> fired_channels;
-    double PE[32];
-    double PE_err[32];
-
-    std::vector<double> l1_fired_time;
-    std::vector<double> l1_fired_pe;
-  };
-
-  struct OpFlashCompare{
-    bool operator() (Opflash *a, Opflash *b) const{
-      if (a->get_time() < b->get_time()){
-	return true;
-      }else{
-	return false;
-      }
-    }
-  };
-
-  typedef std::vector<Opflash*> OpflashSelection;
-  typedef std::set<Opflash*, OpFlashCompare> OpFlashSet;
+  low_time = time - 3 * 15.625/1000.;
+  high_time = time + 37 * 15.625/1000.;
 
 }
 
-#endif
+WireCell::Opflash::Opflash(TH1F **hist, double start_time, int start_bin, int end_bin, float bin_width)
+  : type(2)
+  , flash_id (-1)
+{
+  low_time = start_time + (start_bin+0.5)*bin_width;
+  high_time = start_time + (end_bin-0.5)*bin_width;
+
+  for (int i=0;i!=32;i++){
+    PE[i] = 0;
+    PE_err[i] = 0.2; // 0.2 PE as base ...
+  }
+
+  double max = 0;
+  int max_bin = 0;
+
+  for (int i=start_bin; i!=end_bin;i++){
+    double peak = 0;
+    double mult = 0;
+    for (int j=0;j!=32;j++){
+      double content = hist[j]->GetBinContent(i+1);
+      if (content < 0.2) content = 0;
+      peak += content;
+      PE[j] += content;
+      if (content>1.5) {
+	//	std::cout << i << " " << j << " " << content << std::endl;
+	mult++;
+      }
+    }
+
+    if (peak > max && mult >=3){
+      //std::cout << max << " " << peak << " " << max_bin << " " << i << " " << mult << std::endl;
+      max = peak;
+      max_bin = i;
+    }
+  }
+
+  time = start_time + (max_bin+0.5) * bin_width;
+
+  total_PE = 0;
+  for (int i=0;i!=32;i++){
+    PE[i] -= 1.875; // 7.5 us * random noise ...
+    if (PE[i]<0) PE[i] = 0;
+    total_PE += PE[i];
+
+    if (PE[i]>=2){
+      fired_channels.push_back(i);
+    }
+
+    PE_err[i] = sqrt(pow(PE_err[i],2) // standard error
+		     + (PE[i] + 1.875*2) //statistical term
+		     + pow(PE[i]*0.02,2) // 2% base systematic uncertainties
+		     );
+
+    // if (PE[i] < 1000){
+    //   PE_err[i] = sqrt(pow(PE_err[i],2) + pow(PE[i]*0.02,2)); // standard error below threshold would be 4.6 PE ... 8/sqrt(3)
+    // }else if (PE[i] < 2000){
+    //   PE_err[i] = sqrt(pow(PE_err[i],2) + pow(PE[i]*0.06,2)); // standard error below threshold would be 4.6 PE ... 8/sqrt(3)
+    // }else{
+    //   PE_err[i] = sqrt(pow(PE_err[i],2) + pow(PE[i]*0.18,2)); // standard error below threshold would be 4.6 PE ... 8/sqrt(3)
+    // }
+
+
+  }
+
+}
+
+WireCell::Opflash::~Opflash(){
+
+}
+
+void WireCell::Opflash::swap_channels(){
+  //harded coded for now ...
+  for (auto it = fired_channels.begin(); it!=fired_channels.end(); it++){
+    if (*it == 26) *it = 27;
+    if (*it == 27) *it = 28;
+    if (*it == 28) *it = 29;
+    if (*it == 29) *it = 30;
+    if (*it == 30) *it = 31;
+    if (*it == 31) *it = 26;
+  }
+
+  double temp;
+  temp = PE[31];
+  PE[31] = PE[30];
+  PE[30] = PE[29];
+  PE[29] = PE[28];
+  PE[28] = PE[27];
+  PE[27] = PE[26];
+  PE[26] = temp;
+
+  temp = PE_err[31];
+  PE_err[31] = PE_err[30];
+  PE_err[30] = PE_err[29];
+  PE_err[29] = PE_err[28];
+  PE_err[28] = PE_err[27];
+  PE_err[27] = PE_err[26];
+  PE_err[26] = temp;
+}
+
+void WireCell::Opflash::Add_l1info(TH1F *hist_tot_pe, TH1F *hist_mult, double start_time, int start_bin, int end_bin, float bin_width){
+  std::vector<int> fired_bin;
+  std::vector<double> fired_pe;
+  for (int i=start_bin; i!=end_bin;i++){
+    double pe = hist_tot_pe->GetBinContent(i+1);
+    double mult = hist_mult->GetBinContent(i+1);
+    if (pe >=10 && mult>=3){
+      fired_bin.push_back(i);
+      fired_pe.push_back(pe);
+    }
+  }
+
+  for (size_t i=0; i!=fired_bin.size();i++){
+    if (i==0){
+      double time = start_time + (fired_bin.at(0)+0.5)*bin_width;
+      l1_fired_time.push_back(time);
+      l1_fired_pe.push_back(fired_pe.at(0));
+    }else{
+      if (fired_bin.at(i)-fired_bin.at(i-1)==1){
+	l1_fired_pe.back() += fired_pe.at(i);
+      }else{
+	double time = start_time + (fired_bin.at(i)+0.5)*bin_width;
+	l1_fired_time.push_back(time);
+	l1_fired_pe.push_back(fired_pe.at(i));
+      }
+    }
+  }
+
+  // std::cout << l1_fired_time.size() << std::endl;
+  // for (size_t i = 0; i!= l1_fired_time.size(); i++){
+  //   std::cout << l1_fired_time.at(i) << " " << l1_fired_pe.at(i) << std::endl;
+  // }
+
+}
+
+
+bool WireCell::Opflash::get_fired(int ch){
+  if (std::find(fired_channels.begin(),fired_channels.end(),ch)==fired_channels.end()){
+    return false;
+  }else{
+    return true;
+  }
+}

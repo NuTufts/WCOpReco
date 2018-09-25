e1367881559725150cf753b95da354d2fc018f11
Katie Mason <masonkatie31@gmail.com>
2018-09-25 10:20:42 -0400

    OpWaveform and OpWaveformCollection compile without errors

--- build/include/WCOpReco/OpWaveform.h
+++ build/include/WCOpReco/OpWaveform.h
@@ -1,10 +1,12 @@
 #ifndef OPWAVEFORM_H
 #define OPWAVEFORM_H
 
-class OpWaveform: public std::vector<short> {
-  int ChannelNum
-  float time_from_trigger
-  int type
+#include <vector>
+
+class OpWaveform : public std::vector<short> {
+  int ChannelNum;
+  float time_from_trigger;
+  int type;
 };
 
 #endif
--- build/include/WCOpReco/OpWaveformCollection.h
+++ build/include/WCOpReco/OpWaveformCollection.h
@@ -1,9 +1,11 @@
 #ifndef OPWAVEFORMCOLLECTION_H
 #define OPWAVEFORMCOLLECTION_H
 
+#include <vector>
+
 class OpWaveformCollection : public std::vector<OpWaveform> {
-  void clear_opreco()
-  void pushback_opreco()
+  void clear_opreco();
+  void pushback_opreco();
 };
 
 #endif

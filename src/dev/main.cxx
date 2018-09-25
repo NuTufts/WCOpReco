9d6ead79e781bd3459043328d335306dedf07a65
twongjirad <taritree.wongjirad@gmail.com>
2018-09-25 13:34:28 -0400

    save

---
+++ src/dev/main.cxx
@@ -0,0 +1,21 @@
+#include "WCOpReco/OpWaveform.h"
+#include "WCOpReco/OpWaveformCollection.h"
+#include "WCOpReco/EventOpWaveforms.h"
+#include "WCOpReco/UBEventWaveform.h"
+#include "WCOpReco/DataReader.h"
+
+//root includes
+#include "TH1S.h"
+#include "TF1.h"
+#include "TVirtualFFT.h"
+#include "TTree.h"
+#include "TFile.h"
+
+#include <iostream>
+#include <sstream>
+
+int main(){
+
+  std::cout << "Hello world" << std::endl;
+  void Reader();
+};

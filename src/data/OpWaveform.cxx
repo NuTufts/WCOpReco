56978e3410855e28fa6f5aa652527dc008587c74
Joshua Mills <joshua.mills@tufts.edu>
2018-09-26 10:47:38 -0400

    class changed

--- src/data/OpWaveform.cxx
+++ src/data/OpWaveform.cxx
@@ -1,1 +1,11 @@
 #include "OpWaveform.h"
+//
+//
+// using namespace wcopreco;
+//
+// wcopreco::OpWaveform::OpWaveform(int ChanNum, float time_from_trig, int typ)
+//   {
+//     ChannelNumber = ChanNum;
+//     time_from_trigger = time_from_trig;
+//     type = typ;
+//   }
--- src/data/OpWaveform.h
+++ src/data/OpWaveform.h
@@ -9,13 +9,17 @@ namespace wcopreco {
   public:
     OpWaveform() {};
     virtual ~OpWaveform() {};
-    
+
+    int get_ChannelNum() {return ChannelNum;};
+    float get_time_from_trigger() {return time_from_trigger;};
+    int get_type() {return type;};
+
   protected:
     int ChannelNum;
     float time_from_trigger;
     int type;
   };
-  
+
 }
 
 #endif

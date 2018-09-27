8b78c7fadcd56fd684696a5001af9b354083fa20
Joshua Mills <joshua.mills@tufts.edu>
2018-09-27 12:58:36 -0400

    Quality of life

--- src/data/datareader_j.cxx
+++ src/data/datareader_j.cxx
@@ -17,9 +17,6 @@
 #include "TTimeStamp.h"
 
 
-
-
-
 #include <iostream>
 #include <sstream>
 
@@ -84,45 +81,83 @@ namespace wcopreco {
     std::vector<short> vec(35,8);
 
 
-
+    std::cout << "Remember to Change Entry loop to more than 1! (nevents)" << std::endl;
+    std::cout << "Remember to change each type loop to more than 1! (CHG_NHist)" << std::endl;
     //Here we start looping through events
     for (Int_t i =0; i< 1; i++) {
       // std::cout << i << std::endl;
       tree->GetEntry(i);
-
-      //Fill some Cosmic High Gains
+      //Get Vectors of pmt channel and timestamp for each type of waveform, cosmic/beam and high/low gain
       std::vector<short> CHG_Channel = *cosmic_hg_opch;
       std::vector<double> CHG_Timestamp = *cosmic_hg_timestamp;
-      OpWaveform wfm_CHG(CHG_Channel[0], CHG_Timestamp[0], type, vec);
-
-      //Fill some Cosmic Low Gains
       std::vector<short> CLG_Channel = *cosmic_lg_opch;
       std::vector<double> CLG_Timestamp = *cosmic_lg_timestamp;
-      OpWaveform wfm_CLG(CLG_Channel[0], CLG_Timestamp[0], type, vec);
+      std::vector<short> BHG_Channel = *beam_hg_opch;
+      std::vector<double> BHG_Timestamp = *beam_hg_timestamp;
+      std::vector<short> BLG_Channel = *beam_lg_opch;
+      std::vector<double> BLG_Timestamp = *beam_lg_timestamp;
 
-      // //Fill some Beam High Gains
-      // std::vector<short> BHG_Channel = *beam_hg_opch;
-      // std::vector<double> BHG_Timestamp = *beam_hg_timestamp;
-      // OpWaveform wfm_BHG(BHG_Channel[0], BHG_Timestamp[0], type, vec);
-      //
-      // //Fill some Beam Low Gains
-      // std::vector<short> BLG_Channel = *beam_lg_opch;
-      // std::vector<double> BLG_Timestamp = *beam_lg_timestamp;
-      // OpWaveform wfm_BLG(BLG_Channel[0], BLG_Timestamp[0], type, vec);
-
-      //std::cout << wfm[0] <<"    " <<wfm.get_type() <<"    " <<wfm.get_time_from_trigger() <<"    "<< wfm.get_ChannelNum() << " Did we cout the information?" << std::endl;
-      std::cout << CHG_Channel.size() << "   Channel Size"<<std::endl;
-      std::cout << cosmic_hg_wf->GetSize() <<"    Histogram Size"<< std::endl;
-
+      //Get number of histograms for each type
       TClonesArray CHG_Waveform = *cosmic_hg_wf;
-      Int_t NHist = CHG_Waveform.GetEntries();
-      TClass * Class = CHG_Waveform.GetClass();
+      Int_t CHG_NHist = CHG_Waveform.GetEntries();
+      TClonesArray CLG_Waveform = *cosmic_lg_wf;
+      Int_t CLG_NHist = CLG_Waveform.GetEntries();
+      TClonesArray BHG_Waveform = *beam_hg_wf;
+      Int_t BHG_NHist = BHG_Waveform.GetEntries();
+      TClonesArray BLG_Waveform = *beam_lg_wf;
+      Int_t BLG_NHist = BLG_Waveform.GetEntries();
 
-      TH1S *Hist = (TH1S*)CHG_Waveform.At(1);
-      Int_t Buffer_size = Hist->GetDefaultBufferSize();
-      std::cout  << NHist << "    Number of Histograms in the TClonesArray" << std::endl;
-      std::cout << Buffer_size << "   Is size of buffer in Hist" << std::endl;
-      // Class->Dump();
+      // std::cout << CHG_NHist<< "    " << CLG_NHist<< "    " << BHG_NHist<< "    " << BLG_NHist << std::endl;
+
+
+
+      for (int j = 0; j<1; j++){
+        //Fill some Cosmic High Gains
+        TH1S *CHG_Hist = (TH1S*)CHG_Waveform.At(1);
+        Int_t CHG_nbins = CHG_Hist->GetNbinsX();
+        std::vector<short> test_v(CHG_nbins,0);
+        memcpy(test_v.data(),CHG_Hist->GetArray(), sizeof(short)*CHG_nbins);
+        OpWaveform wfm_CHG(CHG_Channel[0], CHG_Timestamp[0], type, vec);
+        std::cout << "Blah";
+      }
+      for (int j =0; j<1; j++){
+        //Fill some Cosmic Low Gains
+        TH1S *CLG_Hist = (TH1S*)CLG_Waveform.At(1);
+        Int_t CLG_nbins = CLG_Hist->GetNbinsX();
+        std::vector<short> test_v(CLG_nbins,0);
+        memcpy(test_v.data(),CLG_Hist->GetArray(), sizeof(short)*CLG_nbins);
+        OpWaveform wfm_CLG(CLG_Channel[0], CLG_Timestamp[0], type, vec);
+        std::cout << "Blah";
+      }
+      for (int j=0; j<1;j++){
+        //Fill some Beam High Gains
+        TH1S *BHG_Hist = (TH1S*)BHG_Waveform.At(1);
+        Int_t BHG_nbins = BHG_Hist->GetNbinsX();
+        std::vector<short> test_v(BHG_nbins,0);
+        memcpy(test_v.data(),BHG_Hist->GetArray(), sizeof(short)*BHG_nbins);
+        OpWaveform wfm_BHG(BHG_Channel[0], BHG_Timestamp[0], type, vec);
+        std::cout << "Blah";
+      }
+      for (int j=0;j<1;j++){
+        //Fill some Beam Low Gains
+        TH1S *BLG_Hist = (TH1S*)BLG_Waveform.At(1);
+        Int_t BLG_nbins = BLG_Hist->GetNbinsX();
+        std::vector<short> test_v(BLG_nbins,0);
+        memcpy(test_v.data(),BLG_Hist->GetArray(), sizeof(short)*BLG_nbins);
+        OpWaveform wfm_BLG(BLG_Channel[0], BLG_Timestamp[0], type, vec);
+        std::cout << "Blah"<<std::endl;
+      }
+      // std::cout << CHG_Channel.size() << "   Channel Size"<<std::endl;
+      // std::cout << cosmic_hg_wf->GetSize() <<"    Histogram Size"<< std::endl;
+
+
+
+      // for (int x=0; x<test_v.size(); x++) {
+      //   std::cout <<"   " <<CHG_nbins << " " << " "<< test_v.size() << "   Number of bins" << std::endl << "    " <<test_v[x] << std::endl << (CHG_Hist->GetArray())[x] << std::endl;
+        // std::cout  << CHG_NHist << "    Number of Histograms in the TClonesArray" << std::endl;
+        // std::cout << Buffer_size << "   Is size of buffer in CHG_Hist" << std::endl;
+        // Class->Dump();
+      // }
     };
 
 

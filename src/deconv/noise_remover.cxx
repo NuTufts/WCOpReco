bfe5bf166eca7f659d606a8ff530e716d4ab0839
Katie Mason <masonkatie31@gmail.com>
2018-10-03 11:18:49 -0400

    started working on noise remover

--- src/deconv/noise_remover.cxx
+++ src/deconv/noise_remover.cxx
@@ -1,1 +1,11 @@
-//example
+#include "noise_remover.h"
+
+using namespace wcopreco;
+
+noise_remover::noise_remover(int type, int ch, double timestamp)
+  {
+    // OpWaveform = std::vector<short> empty_v(10,0);
+    ChannelNum = ch;
+    time_from_trigger = timestamp;
+    event_type = type;
+  }
--- src/deconv/noise_remover.h
+++ src/deconv/noise_remover.h
@@ -1,2 +1,26 @@
-#ifndef NOISE_REMOVER_H
-#define NOISE_REMOVER_H
+#ifndef NOISE_REMOVER_h
+#define NOISE_REMOVER_h
+
+#include <vector>
+
+namespace wcopreco{
+
+  class noise_remover{
+  public:
+    noise_remover(int type, int ch, double timestamp );
+    virtual ~noise_remover() {};
+
+    int get_type() {return event_type;};
+    int get_channel() {return ChannelNum;};
+    double get_time() {return time_from_trigger;};
+
+  protected:
+    int event_type;
+    double time_from_trigger;
+    int ChannelNum;
+  };
+
+
+}
+
+#endif

2a28c372f8cf9f2dbfa70c78dad1f188c6a5955a
Ralitsa <ralitsa.sharankova@gmail.com>
2018-10-16 16:46:59 -0400

    update configure; now main runs for me

--- test/wirecell/configure.sh
+++ test/wirecell/configure.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-export WC_BASEDIR=$(pwd)
+
+export WC_BASEDIR=$PWD
+export DEVDIR=${WC_BASEDIR}/dev
+
+# OPENCV
+export OPENCV_LIBDIR=/usr/local/lib
+export OPENCV_INCDIR=/usr/local/include
+export USE_OPENCV=1
+
+# setup wc libs
+export WC_LIBDIR=${WC_BASEDIR}
+[[ ":$LD_LIBRARY_PATH:" != *":${WC_LIBDIR}:"* ]] && LD_LIBRARY_PATH="${WC_LIBDIR}:${LD_LIBRARY_PATH}"
+
+# SETUP EIGEN
+export EIGEN_INC_DIR=/usr/include/eigen3
+export EIGEN_LIB_DIR=

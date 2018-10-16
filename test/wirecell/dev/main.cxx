d8e2ff1c132b41b1a7f3e80c217df7a0d03cb4d6
Katie Mason <masonkatie31@gmail.com>
2018-10-16 12:05:06 -0400

    changes to main - doesn't compile!

--- test/wirecell/dev/main.cxx
+++ test/wirecell/dev/main.cxx
@@ -1,7 +1,28 @@
 #include <iostream>
 #include <sstream>
+#include <string>
+
+#include "TApplication.h"
+#include "TCanvas.h"
+#include "TStyle.h"
+#include "TH1F.h"
+#include "TFile.h"
+#include "TGraph2D.h"
+#include "TColor.h"
+#include "TVectorD.h"
+#include "TMatrixD.h"
+
+#include "../ToyLightReco.h"
+
+using namespace WireCell;
+using namespace std;
+
 
 int main(){
   std::cout << "Hello world" << std::endl;
+  const char* file = "celltree.root";
+  //testing ToyLightReco
+  WireCell2dToy::ToyLightReco flash(file,1);
+  flash.load_event_raw(4);
 
 };

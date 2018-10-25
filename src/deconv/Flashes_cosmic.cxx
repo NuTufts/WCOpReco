8223680c700d94619ade157a360776d6a4aa7103
Joshua Mills <joshua.mills@tufts.edu>
2018-10-25 11:43:52 -0400

    Adjusted couts in cosmic flash

--- src/deconv/Flashes_cosmic.cxx
+++ src/deconv/Flashes_cosmic.cxx
@@ -13,13 +13,13 @@ namespace wcopreco {
       Opflash *flash = new Opflash(ophits_group.at(j));
       if (flash->get_total_PE()!=0){
         count++;
-        std::cout << count << " Is now the count of the flashes\n";
+        // std::cout << count << " Is now the count of the flashes\n";
         cosmic_flashes.push_back(flash);
       }
       else{
         delete flash;
         count_d++;
-        std::cout << count_d << " Have been deleted\n";
+        // std::cout << count_d << " Have been deleted\n";
 
       }
       //    std::cout << ophits_group.at(j).size() << " " << flash->get_time() << std::endl;

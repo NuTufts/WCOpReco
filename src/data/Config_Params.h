f3f8a02fb9d1177f3973beeea4d5f7ed9b0cc3ee
Joshua Mills <joshua.mills@tufts.edu>
2018-11-08 10:03:07 -0500

    Started the top level param class

--- src/data/Config_Params.h
+++ src/data/Config_Params.h
@@ -1,87 +1,123 @@
 #ifndef CONFIG_PARAMS_H
 #define CONFIG_PARAMS_H
 
+//Include other Configs:
+#include "Config_COpHit.h"
+#include "Config_Deconvolver.h"
+#include "Config_FlashesBeam.h"
+#include "Config_FlashFiltering.h"
+#include "Config_Hitfinder_Beam.h"
+#include "Config_Hitfinder_Cosmic.h"
+#include "Config_Opflash.h"
+#include "Config_Saturation_Merger.h"
+// #include "Config_UB_rc.h"
+// #include "Config_UB_spe.h"
+//Include basics:
 #include <vector>
 #include <iostream>
 #include <sstream>
 namespace wcopreco {
-  extern int     _num_channels; //lots
-  extern int     _num_types; //lots
-  extern std::vector<int> _nbins_by_type; //lots?
-  extern short   _sat_threshold; //satmerge, cosmic_merger, probably beam_merger
-  extern double  _baseline_default; //scale_lowgains in satmerge
-  extern double  _baseline_difference_max; //scale_lowgains in satmerge, also deconv Remove_Baseline_Secondary
-  extern int     _cosmic_tick_window; //Cosmic merger in satmerge
-  extern float   _tick_width_us;  //cosmic merger in satmerge, Deconvolve_One_Wfm in deconv (1/64) for bin_width and 64 for max_freq_MHz, G(i,k) Hitfinder Beam 15.625/1000, also in Opflash.h and .cxx
-  extern double  _low_bound_baseline_search; //findBaselineLg in SatMerger
-  extern double  _high_bound_baseline_search; //findBaselineLg in SatMerger
-  extern int     _nbins_baseline_search; //find findBaselineLg satmerger
-  extern int     _nbins_saturation_threshold; //satmerger beam and cosmic mergers
-  extern std::vector<float> _scaling_by_channel; // findScaling in satmerger
-  extern double  _high_freq_p0;  //deconv HighFreqFilter
-  extern double  _high_freq_p1;  //deconv HighFreqFilter
-  extern double  _latelight_filter_p0;  //deconv LateLightFilter
-  extern double  _latelight_filter_p1;  //deconv LateLightFilter
-  extern double  _latelight_filter_p2;  //deconv latelight_filter
-  extern double  _baseline_safety_subtraction; // deconv Remove_Baseline_Secondary
-  extern double  _xq; //deconv cal_mean_rms
-  extern double  _xq_diff; //deconv cal_mean_rms
-  extern int     _n_bins_end_wfm; //deconv deconv one wfm end loop 1500-4
-  extern int     _beam_rebin_size; //HitFinder_beam (sort of in opflash.h shows up as 6 where 1500/250 = 6)
-  extern double  _l1_content_thresh; // Hitfinder beam 0.3
-  extern double  _frac_G_t2_first; //Hitfinder beam G
-  extern double  _frac_G_sametime; //Hitfinder beam G
-  extern double  _G_p0; //Hitfinder beam G
-  extern double  _G_p1; //Hitfinder beam G
-  extern double  _G_p2 ; //Hitfinder beam G
-  extern double  _Lasso_p0; //Hitfinder beam
-  extern int     _Lasso_p1; //Hitfinder beam
-  extern double  _Lasso_p2; //Hitfinder beam
-  extern double  _totPE_v_thresh; //Hitfinder beam Also appears in Opflash
-  extern double  _mult_v_thresh; //Hitfinder beam, threshold in PE to determine if add to mult
-  extern double  _l1_mult_v_thresh; //Hitfinder beam
-  extern double  _ophit_group_t_diff_max; // Hitfinder cosmic twice
-  extern double  _bflash_pe_thresh; //Flashes beams
-  extern double  _bflash_mult_thresh; //Flashes beams
-  extern int     _bflash_bin_diff_p0; //Flashes beams 78 is in there 2x
-  extern int     _bflash_bin_diff_p1; //Flashes beams
-  extern int     _bflash_bin_diff_p2; //Flashes beams
-  extern double  _KS_test_thresh; //Flashes beams
-  extern int     _bflash_bin_start_cushion; //Flashes beam
-  extern double  _flash_filter_time_thresh; //Flashfiltering
-  extern double  _flash_filter_pe_thresh; //Flashfiltering
-  extern double  _rc_tau_badch; //UB_rc
-  extern double  _rc_tau_goodch; //UB_rc
-  extern double  _spe_p0; //UB_spe
-  extern double  _spe_p1; //UB_spe
-  extern double  _COphit_integral_thresh; //COPHIT
-  extern double  _COphit_baseline_diff_thresh;//COPHIT
-  extern double  _pe_factor; //COPHIT
-  extern double  _Baseline_uncertainty; //Percent uncertainty COPHIT
-  extern double  _Baseline_unc_bad_baseline; //Percent Unc if baseline bad COPHIT
-  extern double  _cal_integral_p0;  //COPHIT
-  extern double  _cal_integral_p1;  //COPHIT
-  extern double  _cal_integral_p2;  //COPHIT
-  extern double  _cal_integral_p3;  //COPHIT
-  extern double  _cal_integral_p4;  //COPHIT
-  extern double  _cal_integral_p5;  //COPHIT
-  extern double  _PE_err_cosmic; //Opflash
-  extern double  _PE_subtract; //Opflash
-  extern int     _flash_low_time_cushion; //Opflash
-  extern int     _flash_high_time_cushion; //Opflash
-  extern double  _PE_err_beam; //Opflash
-  extern double  _mult_content_thresh; //Opflash
-  extern int     _mult_required; //Opflash
-  extern double  _PE_err_stat_beam;//Opflash
-  extern double  _PE_err_unc_beam;//Opflash
-  extern bool    _do_swap_channels;//Opflash
-  extern double  _addl1_pe_thresh;//Opflash
-  extern double  _addl1_mult_thresh;//Opflash
+    class Config_Params {
+    public:
+      Config_Params();
+      ~Config_Params() {};
 
 
-  //Params NEEDING set:
-  extern std::vector<float> _gain;
-  extern std::vector<float> _gainerror;
+    protected:
+      Config_COpHit             _cfg_cophit ;
+      Config_Deconvolver        _cfg_deconvolver ;
+      Config_FlashesBeam        _cfg_flashesbeam ;
+      Config_FlashFiltering     _cfg_flashfiltering ;
+      Config_Hitfinder_Beam     _cfg_hitfinder_beam ;
+      Config_Hitfinder_Cosmic   _cfg_hitfinder_cosmic ;
+      Config_Opflash            _cfg_opflash ;
+      Config_saturation_merger  _cfg_saturation_merger ;
+
+
+
+    };
+
+  }
+
+  #endif
+
+
+
+  //  int     _num_channels; //lots
+  //  int     _num_types; //lots
+  //  std::vector<int> _nbins_by_type; //lots?
+  //  short   _sat_threshold; //satmerge, cosmic_merger, probably beam_merger
+  //  double  _baseline_default; //scale_lowgains in satmerge
+  //  double  _baseline_difference_max; //scale_lowgains in satmerge, also deconv Remove_Baseline_Secondary
+  //  int     _cosmic_tick_window; //Cosmic merger in satmerge
+  //  float   _tick_width_us;  //cosmic merger in satmerge, Deconvolve_One_Wfm in deconv (1/64) for bin_width and 64 for max_freq_MHz, G(i,k) Hitfinder Beam 15.625/1000, also in Opflash.h and .cxx
+  //  double  _low_bound_baseline_search; //findBaselineLg in SatMerger
+  //  double  _high_bound_baseline_search; //findBaselineLg in SatMerger
+  //  int     _nbins_baseline_search; //find findBaselineLg satmerger
+  //  int     _nbins_saturation_threshold; //satmerger beam and cosmic mergers
+  //  std::vector<float> _scaling_by_channel; // findScaling in satmerger
+  //  double  _high_freq_p0;  //deconv HighFreqFilter
+  //  double  _high_freq_p1;  //deconv HighFreqFilter
+  //  double  _latelight_filter_p0;  //deconv LateLightFilter
+  //  double  _latelight_filter_p1;  //deconv LateLightFilter
+  //  double  _latelight_filter_p2;  //deconv latelight_filter
+  //  double  _baseline_safety_subtraction; // deconv Remove_Baseline_Secondary
+  //  double  _xq; //deconv cal_mean_rms
+  //  double  _xq_diff; //deconv cal_mean_rms
+  //  int     _n_bins_end_wfm; //deconv deconv one wfm end loop 1500-4
+  //  int     _beam_rebin_size; //HitFinder_beam (sort of in opflash.h shows up as 6 where 1500/250 = 6)
+  //  double  _l1_content_thresh; // Hitfinder beam 0.3
+  //  double  _frac_G_t2_first; //Hitfinder beam G
+  //  double  _frac_G_sametime; //Hitfinder beam G
+  //  double  _G_p0; //Hitfinder beam G
+  //  double  _G_p1; //Hitfinder beam G
+  //  double  _G_p2 ; //Hitfinder beam G
+  //  double  _Lasso_p0; //Hitfinder beam
+  //  int     _Lasso_p1; //Hitfinder beam
+  //  double  _Lasso_p2; //Hitfinder beam
+  //  double  _totPE_v_thresh; //Hitfinder beam Also appears in Opflash
+  //  double  _mult_v_thresh; //Hitfinder beam, threshold in PE to determine if add to mult
+  //  double  _l1_mult_v_thresh; //Hitfinder beam
+  //  double  _ophit_group_t_diff_max; // Hitfinder cosmic twice
+  //  double  _bflash_pe_thresh; //Flashes beams
+  //  double  _bflash_mult_thresh; //Flashes beams
+  //  int     _bflash_bin_diff_p0; //Flashes beams 78 is in there 2x
+  //  int     _bflash_bin_diff_p1; //Flashes beams
+  //  int     _bflash_bin_diff_p2; //Flashes beams
+  //  double  _KS_test_thresh; //Flashes beams
+  //  int     _bflash_bin_start_cushion; //Flashes beam
+  //  double  _flash_filter_time_thresh; //Flashfiltering
+  //  double  _flash_filter_pe_thresh; //Flashfiltering
+  //  double  _rc_tau_badch; //UB_rc
+  //  double  _rc_tau_goodch; //UB_rc
+  //  double  _spe_p0; //UB_spe
+  //  double  _spe_p1; //UB_spe
+  //  double  _COphit_integral_thresh; //COPHIT
+  //  double  _COphit_baseline_diff_thresh;//COPHIT
+  //  double  _pe_factor; //COPHIT
+  //  double  _Baseline_uncertainty; //Percent uncertainty COPHIT
+  //  double  _Baseline_unc_bad_baseline; //Percent Unc if baseline bad COPHIT
+  //  double  _cal_integral_p0;  //COPHIT
+  //  double  _cal_integral_p1;  //COPHIT
+  //  double  _cal_integral_p2;  //COPHIT
+  //  double  _cal_integral_p3;  //COPHIT
+  //  double  _cal_integral_p4;  //COPHIT
+  //  double  _cal_integral_p5;  //COPHIT
+  //  double  _PE_err_cosmic; //Opflash
+  //  double  _PE_subtract; //Opflash
+  //  int     _flash_low_time_cushion; //Opflash
+  //  int     _flash_high_time_cushion; //Opflash
+  //  double  _PE_err_beam; //Opflash
+  //  double  _mult_content_thresh; //Opflash
+  //  int     _mult_required; //Opflash
+  //  double  _PE_err_stat_beam;//Opflash
+  //  double  _PE_err_unc_beam;//Opflash
+  //  bool    _do_swap_channels;//Opflash
+  //  double  _addl1_pe_thresh;//Opflash
+  //  double  _addl1_mult_thresh;//Opflash
+  // //Params NEEDING set:
+  //  std::vector<float> _gain;
+  //  std::vector<float> _gainerror;
 
 
   // void _set_num_channels(int n){_num_channels = n;}
@@ -286,6 +322,3 @@ namespace wcopreco {
   //
   // void _set_addl1_mult_thresh(double value) {_addl1_mult_thresh = value;}
   // double _get_addl1_mult_thresh() {return _addl1_mult_thresh;}
-  }
-
-  #endif
--- src/data/Config_Saturation_Merger.cxx
+++ src/data/Config_Saturation_Merger.cxx
@@ -1,8 +1,8 @@
-#include "Config_saturation_merger.h"
+#include "Config_Saturation_Merger.h"
 
 namespace wcopreco {
 
-  Config_saturation_merger::Config_saturation_merger()
+  Config_saturation_merger::Config_Saturation_Merger()
     {
        _num_channels = 32 ;
        _sat_threshold = 4080 ;
@@ -17,7 +17,7 @@ namespace wcopreco {
        _scaling_by_channel =  {10.13, 10.20, 10.13, 10.05, 9.96, 9.95, 10.04, 9.58, 9.42, 9.81, 9.25, 9.61, 9.56, 9.35, 9.99, 9.66, 10.26, 9.82, 10.32, 10.08, 9.77, 9.64, 10.14, 9.74, 9.76, 10.10, 10.48, 9.81, 9.99, 9.79, 9.93, 10.01};
      }
 
-     void Config_saturation_merger::_set_scaling_by_channel(std::vector<float> scalings_v) {
+     void Config_Saturation_Merger::_set_scaling_by_channel(std::vector<float> scalings_v) {
        _scaling_by_channel = scalings_v;
        if (scalings_v.size() != _num_channels) {
          std::cout << "Careful, size of scaling vector is not the same as the number of channels currently set (try to set _num_channels first if you think they should be the same).\n";

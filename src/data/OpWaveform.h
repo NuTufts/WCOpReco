9f670dbb075a1258725d45f2ebdefa859469ec00
Ralitsa Sharankova <ralitsa.sharankova@gmail.com>
2018-11-20 08:43:26 -0600

    const getters

--- src/data/OpWaveform.h
+++ src/data/OpWaveform.h
@@ -12,9 +12,9 @@ namespace wcopreco {
     OpWaveform(int ChNum, double time_from_trig, int typ, const int nelems );
     virtual ~OpWaveform() {};
 
-    int get_ChannelNum() {return ChannelNum;};
-    double get_time_from_trigger() {return time_from_trigger;};
-    int get_type() {return type;};
+    int get_ChannelNum() const {return ChannelNum;};
+    double get_time_from_trigger() const {return time_from_trigger;};
+    int get_type() const {return type;};
     void set_ChannelNum(int ch) {ChannelNum = ch;};
     void set_time_from_trigger(double time_diff) {time_from_trigger = time_diff;};
     void set_type(int typ) {type = typ;};
